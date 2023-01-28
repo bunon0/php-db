6865bbec9421755d45b0a1cc53fe39e96282fcb4
bunon0 <86172592+bunon0@users.noreply.github.com>
2023-01-28 13:23:48 +0900

    headerとfooterのパーツ化

--- index.php
+++ index.php
@@ -12,18 +12,8 @@ try {
 }
 ?>
 
-<!DOCTYPE html>
-<html lang="ja">
+<?php require_once(__DIR__ . '/layouts/header.php'); ?>
 
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>シンプルPHP+DB</title>
-</head>
+<main class="p-index"></main>
 
-<body>
-  <h1></h1>
-</body>
-
-</html>
+<?php require_once(__DIR__ . '/layouts/footer.php'); ?>
