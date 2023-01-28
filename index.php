b50da33c93eb34a62602ef3d7b7e366047767b46
bunon0 <86172592+bunon0@users.noreply.github.com>
2023-01-28 17:43:35 +0900

    topページに各ページへのlinkを追加

--- index.php
+++ index.php
@@ -4,8 +4,6 @@ $db_pass = getenv('DB_PASSWORD');
 
 try {
   $dbh = new PDO('mysql:localhost;dbname=php_db', $db_user, $db_pass);
-  echo 'DB接続に成功しました';
-
   $dbh = null;
 } catch (PDOException $e) {
   exit('DB接続に失敗しました。' . $e->getMessage());
@@ -15,7 +13,19 @@ try {
 <?php require_once(__DIR__ . '/layouts/header.php'); ?>
 
 <main class="l-main">
-  <div class="p-index"></div>
+  <div class="l-main__inner">
+    <div class="p-top">
+      <h2 class="p-top__title">トップページ</h2>
+      <div class="p-top-article">
+        <article class="p-top-article__card">
+          <h3 class="p-top-article__title">SElECT文でDBの値を取得して表示</h3>
+          <div class="p-top-article__link">
+            <a href="./select.php" class="p-top-article__link-inline">Link Page</a>
+          </div>
+        </article>
+      </div>
+    </div>
+  </div>
 </main>
 
 <?php require_once(__DIR__ . '/layouts/footer.php'); ?>
