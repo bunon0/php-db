973423a4fbeaa18a135e64a3b27945565ee86203
bunon0 <86172592+bunon0@users.noreply.github.com>
2023-01-28 11:52:17 +0900

    DB接続テスト #1

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
+<?php
+$db_user =  getenv('DB_USERNAME');
+$db_pass = getenv('DB_PASSWORD');
+
+try {
+  $dbh = new PDO('mysql:localhost;dbname=php_db', $db_user, $db_pass);
+  echo 'DB接続に成功しました';
+
+  $dbh = null;
+} catch (PDOException $e) {
+  exit('DB接続に失敗しました。' . $e->getMessage());
+}
+?>
+
 <!DOCTYPE html>
 <html lang="ja">
 
@@ -5,11 +19,11 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <title>シンプルPHP+DB</title>
 </head>
 
 <body>
-  <h1>devtest</h1>
+  <h1></h1>
 </body>
 
 </html>
