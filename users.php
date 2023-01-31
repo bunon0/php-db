70a2c8c36e27ee2455614fd984a488a7460846b1
bunon0 <86172592+bunon0@users.noreply.github.com>
2023-01-31 09:16:18 +0900

    不要なコードの削除 #7

--- users.php
+++ users.php
@@ -18,41 +18,31 @@ try {
 
 ?>
 
-<!DOCTYPE html>
-<html lang="ja">
+<?php require_once(__DIR__ . '/layouts/header.php'); ?>
 
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
-</head>
+<main class="l-main">
 
-<body>
-  <?php require_once(__DIR__ . '/layouts/header.php'); ?>
-  <main class="l-main">
-
-    <div class="l-main__inner">
-      <div class="p-users">
-        <h2 class="p-users__title">ユーザー情報一覧</h2>
-        <div class="p-users__table">
-          <table class="c-table01">
-            <thead class="c-table01__thead">
-              <tr class="c-table01__row">
-                <th class="c-table01__th">ID</th>
-                <th class="c-table01__th">氏名</th>
-                <th class="c-table01__th">ふりがな</th>
-                <th class="c-table01__th">メールアドレス</th>
-                <th class="c-table01__th">年齢</th>
-                <th class="c-table01__th">住所</th>
-                <th class="c-table01__th">編集</th>
-              </tr>
-            </thead>
-            <tbody class="c-table01__tbody">
-              <!-- データベースから取得してきたuser情報をテーブル列として表示 -->
-              <?php
-              foreach ($users as $user) {
-                echo "
+  <div class="l-main__inner">
+    <div class="p-users">
+      <h2 class="p-users__title">ユーザー情報一覧</h2>
+      <div class="p-users__table">
+        <table class="c-table01">
+          <thead class="c-table01__thead">
+            <tr class="c-table01__row">
+              <th class="c-table01__th">ID</th>
+              <th class="c-table01__th">氏名</th>
+              <th class="c-table01__th">ふりがな</th>
+              <th class="c-table01__th">メールアドレス</th>
+              <th class="c-table01__th">年齢</th>
+              <th class="c-table01__th">住所</th>
+              <th class="c-table01__th">編集</th>
+            </tr>
+          </thead>
+          <tbody class="c-table01__tbody">
+            <!-- データベースから取得してきたuser情報をテーブル列として表示 -->
+            <?php
+            foreach ($users as $user) {
+              echo "
                 <tr class='c-table01__row'>
                   <td class='c-table01__td'>{$user['id']}</td>
                   <td class='c-table01__td'>{$user['name']}</td>
@@ -65,23 +55,20 @@ try {
                   </td>
                 </tr>
               ";
-              }
-              ?>
-            </tbody>
-          </table>
-          <!-- /.c-table -->
-        </div>
-        <!-- /.p-users__table -->
-        <div class="p-users__btn-back">
-          <a href="./index.php" class="c-btn-back">トップへ戻る</a>
-        </div>
+            }
+            ?>
+          </tbody>
+        </table>
+        <!-- /.c-table -->
       </div>
-      <!-- /.p-users -->
+      <!-- /.p-users__table -->
+      <div class="p-users__btn-back">
+        <a href="./index.php" class="c-btn-back">トップへ戻る</a>
+      </div>
     </div>
-    <!-- /.l-main__inner -->
-  </main>
+    <!-- /.p-users -->
+  </div>
+  <!-- /.l-main__inner -->
+</main>
 
-  <?php require_once(__DIR__ . '/layouts/footer.php'); ?>
-</body>
-
-</html>
+<?php require_once(__DIR__ . '/layouts/footer.php'); ?>
